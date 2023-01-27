b479b00f11cbad15c702aab4ae285c2be52fda0e
danmasani12 <danmasani12@gmail.com>
2023-01-26 23:57:23 -0800

    New update

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,9 @@ from .import views
 
 
 urlpatterns =[
-    path('', views.index, name= 'index')
+    path('', views.index, name= 'index'),
+    path('post/', views.post, name= 'post'),
+    path('homepage/', views.HomeView.as_view(), name= 'homepage'),
+    path('room/<int:pk>/', views.portfolio, name= 'portfolio'),
+    path('send/<int:pk>/', views.send_message, name= 'send_message'),
 ]
